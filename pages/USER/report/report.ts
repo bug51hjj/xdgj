921e5b931c1c7a0432fe15465836700c38f8997d
minson <512141260@qq.com>
2018-05-11 11:23:31 +0800

    Revert "Revert "2018051101""
    This reverts commit 7b194c92db883f546176f8724a2ad4c41cc9fb07.

--- pages/USER/report/report.ts
+++ pages/USER/report/report.ts
@@ -1,69 +1,76 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams,AlertController,LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 import { HttpServiceProvider } from '../../../providers/http-service/http-service';
 import { GetDateProvider } from '../../../providers/get-date/get-date';
+import { UserBetedListPage } from '../../../pages/USER/user-beted-list/user-beted-list'; //用户中心-已结算货单
 @IonicPage()
 @Component({
-  selector: 'page-report',
-  templateUrl: 'report.html',
+    selector: 'page-report',
+    templateUrl: 'report.html',
 })
 export class ReportPage {
-    public tabActive:string = 'today'; //yesterday week lastWeek 
-    public report:any = {
-        loading:true,
-        status:false,
-        list:[]
+    public tabActive: string = 'today'; //yesterday week lastWeek 
+    public UserBetedListPage: any = UserBetedListPage;
+    public report: any = {
+        loading: true,
+        status: false,
+        list: []
     };
-    constructor(public navCtrl: NavController, 
-      public navParams: NavParams,
-      public HttpService:HttpServiceProvider,
-      public loadingCtrl: LoadingController,
-      public alertCtrl: AlertController,
-      public getDate:GetDateProvider) {
+    constructor(public navCtrl: NavController,
+        public navParams: NavParams,
+        public HttpService: HttpServiceProvider,
+        public loadingCtrl: LoadingController,
+        public alertCtrl: AlertController,
+        public getDate: GetDateProvider) {
     }
 
     ionViewDidLoad() {
-      this.getReportData('today')
+        this.getReportData('today')
     }
-    changeTab(type){
+    changeTab(type) {
         this.tabActive = type;
         this.getReportData(type);
     }
-    getReportData(type){
+    queryCurrentOrder(order) {
+        this.navCtrl.push(UserBetedListPage, { date: order.date })
+    }
+    getReportData(type) {
         let token = window.localStorage.getItem('token');
-        let date_start,date_end;
-        if(type=='today'){
+        let date_start, date_end;
+        if (type == 'today') {
             date_start = this.getDate.today();
             date_end = this.getDate.today();
-        }else if(type=='yesterday'){
+        } else if (type == 'yesterday') {
             date_start = this.getDate.yesterday();
             date_end = this.getDate.yesterday();
-        }else if(type=='week'){
+        } else if (type == 'week') {
             date_start = this.getDate.weekStartDate();
             date_end = this.getDate.today();
-        }else if(type=='lastWeek'){
+        } else if (type == 'lastWeek') {
             date_start = this.getDate.lastWeekStartDate();
             date_end = this.getDate.lastWeekEndDate();
-        }else{ return false}
+        } else { return false }
         let url = `/report/query?date_start=${date_start}&date_end=${date_end}&tk=${token}`;
         this.report.loading = true;
+        let loader = this.loadingCtrl.create({ content: "数据加载中..." });
+		loader.present();
         this.HttpService.get(url).subscribe((res: Response) => {
-            if(res['errorcode']==''){
+            loader.dismiss();
+            if (res['errorcode'] == 0) {
                 this.report.loading = false;
-                if(res['report'].length>0){
+                if (res['report'].length > 0) {
                     this.report.list = res['report'];
                     this.report.status = true;
-                }else{
+                } else {
                     this.report.status = false;
                 }
-            }else{
-              let alert = this.alertCtrl.create({
-                subTitle: res['errormsg'],
-                  buttons: ['确认']
+            } else {
+                let alert = this.alertCtrl.create({
+                    subTitle: res['errormsg'],
+                    buttons: ['确认']
                 });
                 alert.present();
             }
         })
-        console.log(date_start,date_end)
     }
 }
